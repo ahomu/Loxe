a27418eacf2f745e37bd18ff927dda2e6fd3d550
ahomu <mail@ayumusato.com>
2015-06-09 09:28:23 +0900

    remove old tests

--- src/classes/__test__/Store.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import assert from 'power-assert';
-import React from 'react/dist/react-with-addons';
-import Store from '../Store';
-
-describe('Store', ()=> {
-  it('not need...', ()=> {});
-});
